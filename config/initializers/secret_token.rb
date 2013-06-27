1d17aa458724a0ffd01647b5ab84017b2d66d70d
Robert Walker <robert4723@me.com>
2013-06-26 20:27:46 -0400

    WIP: Upgrading to Rails 4.0.0.
    Regenerating app by running rails new chatl3 over the existing project and merging changes where necessary.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Chatl3::Application.config.secret_token = 'c917c0bea19db32af48cbfbaee407313b911afc7150860e78d3f85787e002bd6964839a8afc7e85896baeccb4431b2f6bfa7452c3edeb56dadfe47466c4b6a9b'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatl3::Application.config.secret_key_base = '1fa9bd4e8faef5d54345a68be910fa7b22c3a11e62dcc51bcc9b5ca6108a252eb0c1389998035a85f47ff38b09653baab883eb63f047cd6fea6cbb0357a0a829'
