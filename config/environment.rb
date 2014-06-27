0be074cc9f956a2c21d9892258bbcb97b3d564d8
Robert Walker <robert4723@me.com>
2014-06-26 20:47:51 -0400

    Upgrading to Rails 4.1.2

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Chatl3::Application.initialize!
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Chatl3::Application.config.session_store :cookie_store, key: '_chatl3_session'
+Rails.application.config.session_store :cookie_store, key: '_chatl3_session'
